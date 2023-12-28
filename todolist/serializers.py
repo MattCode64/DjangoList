5cf2e2ccafbff5c6393482817031aff84694d7f3
MattKio <matthieu.freire.eleuterio@efrei.net>
2023-12-28 11:32:40 +0100

    models.py / serializers.py / makemigrations and migrate

---
+++ todolist/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Task, Collection
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = '__all__'
+        # fields = ['id', 'title', 'slug']
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+        # fields = ['id', 'title', 'slug', 'description', 'completed', 'created', 'collection']
