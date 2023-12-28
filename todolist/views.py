338a9695bee88a793ede3d8e332e045d90eb2f8d
MattKio <matthieu.freire.eleuterio@efrei.net>
2023-12-28 11:57:44 +0100

    views.py declaration

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Collection, Task
+from .serializers import CollectionSerializer, TaskSerializer
+
 
 # Create your views here.
+
+class CollectionList(generics.ListAPIView):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+
+class TaskList(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class CollectionDetail(generics.RetrieveAPIView):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+
+class TaskDetail(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
