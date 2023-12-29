28967358c53d59e33e18d6acc8c930ae49874ae8
MattKio <matthieu.freire.eleuterio@efrei.net>
2023-12-29 16:59:12 +0100

    Units Tests for endpoints

--- todolist/tests.py
+++ todolist/tests.py
@@ -6,44 +6,46 @@ from .models import Collection, Task
 
 # Create your tests here.
 
-def test_success(nb=0):
-    print("\033[93m" + " ~~~ TEST " + str(nb) + " SUCCESS ~~~ " + "\033[0m")
+def test_success(test_name):
+    # Print the test name in yellow
+    print("\033[93m" + " ~~~ TEST " + test_name + " SUCCESS ~~~ " + "\033[0m")
 
 
 class CollectionAPITests(APITestCase):
     def setUp(self):
         self.collection = Collection.objects.create(title="Test Collection")
 
-    def test_list_collections(self):
-        url = reverse('collection-list')
-        response = self.client.retrieve(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        test_success(1)
-
-    def test_detail_collection_success(self):
-        url = reverse('collection-detail', args=[self.collection.id])
-        response = self.client.retrieve(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        test_success(2)
-
-    def test_detail_collection_not_found(self):
-        url = reverse('collection-detail', args=[999])  # Un ID qui n'existe pas
-        response = self.client.retrieve(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        test_success(3)
-
     def test_create_collection(self):
         url = reverse('collection-list')
-        data = {'title': 'New Collection Test'}
+        data = {'title': 'New Test Collection Created'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        test_success(4)
+        test_success("COLLECTION CREATE")
 
-    def test_delete_collection(self):
-        url = reverse('collection-detail', args=[self.collection.id])
-        response = self.client.delete(url, format='json')
+    def test_list_collection(self):
+        url = reverse('collection-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        test_success("COLLECTION LIST")
+
+    def test_destroy_collection(self):
+        url = reverse('collection-detail', kwargs={'collection_pk': self.collection.pk})
+        response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        test_success(8)
+        test_success("COLLECTION DELETE")
+
+    def test_retrieve_collection(self):
+        url = reverse('collection-detail', kwargs={'collection_pk': self.collection.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        test_success("COLLECTION RETRIEVE")
+
+    def test_update_collection(self):
+        url = reverse('collection-detail', kwargs={'collection_pk': self.collection.pk})
+        data = {'title': 'Test Collection Updated'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        test_success("COLLECTION UPDATE")
 
 
 class TaskAPITests(APITestCase):
@@ -51,20 +53,36 @@ class TaskAPITests(APITestCase):
         self.collection = Collection.objects.create(title="Test Collection")
         self.task = Task.objects.create(title="Test Task", collection=self.collection)
 
-    def test_list_tasks(self):
-        url = reverse('task-list')
-        response = self.client.retrieve(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        test_success(5)
+    def test_create_task(self):
+        url = reverse('tasks-list', kwargs={'collection_pk': self.collection.pk})
+        data = {'title': 'New Test Task Created', 'collection': self.collection.pk}
+        response = self.client.post(url, data, format='json')
+        if response.status_code != status.HTTP_201_CREATED:
+            print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        test_success("TASK CREATE")
 
-    def test_detail_task_success(self):
-        url = reverse('task-detail', args=[self.task.id])
-        response = self.client.retrieve(url, format='json')
+    def test_list_task(self):
+        url = reverse('tasks-list', kwargs={'collection_pk': self.collection.pk})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        test_success(6)
+        test_success("TASK LIST")
 
-    def test_detail_task_not_found(self):
-        url = reverse('task-detail', args=[999])
-        response = self.client.retrieve(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        test_success(7)
+    def test_destroy_task(self):
+        url = reverse('task-detail', kwargs={'task_pk': self.task.pk, 'collection_pk': self.collection.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        test_success("TASK DELETE")
+
+    def test_retrieve_task(self):
+        url = reverse('task-detail', kwargs={'task_pk': self.task.pk, 'collection_pk': self.collection.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        test_success("TASK RETRIEVE")
+
+    def test_update_task(self):
+        url = reverse('task-detail', kwargs={'task_pk': self.task.pk, 'collection_pk': self.collection.pk})
+        data = {'title': 'Test Task Updated', 'collection': self.collection.pk}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        test_success("TASK UPDATE")
